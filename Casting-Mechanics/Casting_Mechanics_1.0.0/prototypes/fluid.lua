aa97ee8962c5695360270812cda5e2ea4db543f0
Michael <mbiskup2608@gmail.com>
2025-03-11 15:49:08 -0400

    Update fluid.lua

--- Casting-Mechanics/Casting_Mechanics_1.0.0/prototypes/fluid.lua
+++ Casting-Mechanics/Casting_Mechanics_1.0.0/prototypes/fluid.lua
@@ -11,5 +11,18 @@ data:extend(
     flow_color = {0.8, 1, 0.1},
     icon = "__micaalle_Graphics__/graphics/items/chlorine-gas.PNG",
     order = "a[fluid]-a[chlorine-gas]"
-  }
+  },
+  {
+    type = "fluid",
+    name = "molten-silicon",
+    icon = "__micaalle_Graphics__/graphics/items/molten-silicon.png",
+    subgroup = "fluid",
+    order = "b[new-fluid]-a[molten-silicon]",
+    default_temperature = 1500,
+    max_temperature = 2000,
+    heat_capacity = "0.01kJ",
+    base_color = {0.1, 0.4, 0.35},
+    flow_color = {0.3, 0.6, 0.6},
+    auto_barrel = false
+  },
 })
